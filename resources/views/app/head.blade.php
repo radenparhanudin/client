89163a0f230a7734da50aa9834d763b530af7971
Raden Parhanudin <radenparhanudin@gmail.com>
2020-09-28 13:34:35 +0700

    Update Assets

--- resources/views/app/head.blade.php
+++ resources/views/app/head.blade.php
@@ -8,25 +8,25 @@
    <meta content="Raden Parhanudin" name="author" />
    <title>{{ config('app.name') }}</title>
    <!-- Bootstrap Core CSS -->
-   <link href="{{ asset('startmin') }}/css/bootstrap.min.css" rel="stylesheet">
-   <link rel="stylesheet" href="{{ asset('startmin/css/bs4.css') }}">
+   <link href="{{ asset('public/startmin') }}/css/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" href="{{ asset('public/startmin/css/bs4.css') }}">
    <!-- MetisMenu CSS -->
-   <link href="{{ asset('startmin') }}/css/metisMenu.min.css" rel="stylesheet">
+   <link href="{{ asset('public/startmin') }}/css/metisMenu.min.css" rel="stylesheet">
    <!-- Timeline CSS -->
-   <link href="{{ asset('startmin') }}/css/timeline.css" rel="stylesheet">
+   <link href="{{ asset('public/startmin') }}/css/timeline.css" rel="stylesheet">
    <!-- Custom CSS -->
-   <link href="{{ asset('startmin') }}/css/startmin.css" rel="stylesheet">
+   <link href="{{ asset('public/startmin') }}/css/startmin.css" rel="stylesheet">
    <!-- Morris Charts CSS -->
-   <link href="{{ asset('startmin') }}/css/morris.css" rel="stylesheet">
+   <link href="{{ asset('public/startmin') }}/css/morris.css" rel="stylesheet">
    <!-- Custom Fonts -->
-   <link href="{{ asset('startmin') }}/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+   <link href="{{ asset('public/startmin') }}/css/font-awesome.min.css" rel="stylesheet" type="text/css">
    <!-- DataTables CSS -->
-     <link href="{{ asset('startmin') }}/css/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+     <link href="{{ asset('public/startmin') }}/css/dataTables/dataTables.bootstrap.css" rel="stylesheet">
      <!-- DataTables Responsive CSS -->
-     <link href="{{ asset('startmin') }}/css/dataTables/dataTables.responsive.css" rel="stylesheet">
+     <link href="{{ asset('public/startmin') }}/css/dataTables/dataTables.responsive.css" rel="stylesheet">
         
-   <link rel="stylesheet" href="{{ asset('startmin/plugins/select2/dist/css/select2.min.css') }}">
-   <link rel="stylesheet" href="{{ asset('startmin/plugins/toastr/toastr.min.css') }}">
+   <link rel="stylesheet" href="{{ asset('public/startmin/plugins/select2/dist/css/select2.min.css') }}">
+   <link rel="stylesheet" href="{{ asset('public/startmin/plugins/toastr/toastr.min.css') }}">
    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --> <!-- WARNING: Respond.js doesn't work if you view the page via file:// --> <!--[if lt IE 9]>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script> <![endif]-->
